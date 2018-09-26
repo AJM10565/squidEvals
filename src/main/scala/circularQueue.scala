887474017e64bc3a41b56db64112576087bb9590
amiller17 <amiller17@luc.edy>
2018-09-26 00:24:01 -0500

    Java to Scala: Argsparser and Testargsparser Tests Pass

--- src/main/scala/circularQueue.scala
+++ src/main/scala/circularQueue.scala
@@ -2,36 +2,30 @@ package wf
 
 
 class circularQueue(val parser: argsparser, var holder: wordHolder, var acceptor: cloudAcceptor) {
-  buffermax = parser.getLastnwords
-  myStringArray = new Array[String](buffermax)
-  position = 0
-  minlength = parser.getMinlength
-  waitcount = parser.getWaitcount
-  count = 0
-  bufferinit = 0
-  private var position = 0
-  private var myStringArray = null
-  private var minlength = 0
-  private var waitcount = 0
-  private var count = 0
-  private var buffermax = 0
-  private var bufferinit = 0
+  var buffermax = parser.getLastnwords
+  var myStringArray = new Array[String](buffermax)
+  var position = 0
+  var minlength = parser.getMinlength
+  var waitcount = parser.getWaitcount
+  var count = 0
+  var bufferinit = 0
+
 
   def add(new_word: String): Unit = {
-  if (myStringArray(position) != null) {
-  val old_word = myStringArray(position)
-  if (old_word.length >= minlength) holder.decrement(old_word)
-  }
-  myStringArray(position) = new_word
-  if (new_word.length >= minlength) holder.increment(new_word)
-  position += 1
-  if (position >= buffermax) position = 0
-  count += 1
-  bufferinit += 1
-  if (bufferinit >= buffermax) if (count >= waitcount) {
-  count = 0
-  acceptor.accept(holder.createoutput)
-  }
+    if (myStringArray(position) != null) {
+    val old_word = myStringArray(position)
+    if (old_word.length >= minlength) holder.decrement(old_word)
+    }
+    myStringArray(position) = new_word
+    if (new_word.length >= minlength) holder.increment(new_word)
+    position += 1
+    if (position >= buffermax) position = 0
+    count += 1
+    bufferinit += 1
+    if (bufferinit >= buffermax) if (count >= waitcount) {
+    count = 0
+    acceptor.accept(holder.createoutput)
+    }
   }
 }
 
